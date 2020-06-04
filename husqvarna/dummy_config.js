063ebe468396a0e8457b9fc0dde0c6751512ca25
Marc-André Weber <mcweba@users.noreply.github.com>
2020-06-04 23:21:33 +0200

    Create dummy_config.js

---
+++ husqvarna/dummy_config.js
@@ -0,0 +1,11 @@
+const refresh_token = 'XXXXXXXXXXXXXXXXXXXXXX'
+const client_id = 'XXXXXXXXXXXXXXXXXXXXXX'
+const x_api_key = 'XXXXXXXXXXXXXXXXXXXXXX'
+const mower_id = 'XXXXXXXXXXXXXXXXXXXXXX'
+
+module.exports = {
+    refresh_token,
+    client_id,
+    x_api_key,
+    mower_id
+}
